4a17b5302357078e8c408e52dc74252ca4c355ac
kgawerda <konradgawerda@gmail.com>
2022-11-28 13:24:43 +0100

    pages deploy test

--- main.js
+++ main.js
@@ -4,8 +4,7 @@ var __webpack_exports__ = {};
   !*** ./src/index.js ***!
   \**********************/
 
-console.log("fuck this")
-alert("yes it does work");
+console.log("fuck my life")
 /******/ })()
 ;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7QUFDQTtBQUNBLDBCIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdG9kb2xpc3QvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiXG5jb25zb2xlLmxvZyhcImZ1Y2sgdGhpc1wiKVxuYWxlcnQoXCJ5ZXMgaXQgZG9lcyB3b3JrXCIpOyJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7QUFDQSwyQiIsInNvdXJjZXMiOlsid2VicGFjazovL3RvZG9saXN0Ly4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbIlxuY29uc29sZS5sb2coXCJmdWNrIG15IGxpZmVcIikiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
