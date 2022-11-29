4eab1a4e3f0636f0e61905a8dd831922cf739823
kgawerda <konradgawerda@gmail.com>
2022-11-29 01:01:30 +0100

    fix adding tasks

--- src/dom_functions.js
+++ src/dom_functions.js
@@ -85,7 +85,7 @@ function renderProject(project) {
     const addbtn = document.createElement('button');
 
     addbtn.classList.add('add-task-btn');
-    
+
     addbtn.textContent = '+ Add task';
     projectDiv.appendChild(addbtn);
 
@@ -180,19 +180,38 @@ function derenderTasks() {
 }
 
 function renderForm(project) {
-    const form=document.querySelector('#task-form');
-    form.style.display='block';
-    form.onsubmit = function(){
+    const form = document.querySelector('#task-form');
+    const formbtn = document.querySelector('.form-btn');
+    form.style.display = 'block';
+    formbtn.addEventListener('click', (e) => {
+        e.preventDefault();
         const name = form.elements['name'].value;
         const description = form.elements['description'].value;
         const date = form.elements['date'].value;
         const priority = parseInt(form.elements['priority'].value);
         const done = form.elements['done'].checked;
-        let task=new Task(name,description,new Date(date), priority, done);
+        let task = new Task(name, description, new Date(date), priority, done);
         project.addTask(task);
         createTask(task);
-        form.style.display='none';
-    };
+        addEventListenerToTask(task,project);
+        form.style.display = 'none';
+    });
 }
 
-export default renderProject
+function firstRender() {
+    let task1 = new Task('A Do something', 'description1', new Date(2022, 10, 30), 2, false);
+    let task2 = new Task('B Do something', 'description2', new Date(2022, 10, 28), 0, true);
+    let task3 = new Task('D Do something', 'description3', new Date(2022, 0, 2), 1, false);
+    let task4 = new Task('C Do something', 'description4', new Date(2022, 11, 2), 0, false);
+
+    let project = new Project('projekty');
+
+    project.addTask(task1);
+    project.addTask(task2);
+    project.addTask(task3);
+    project.addTask(task4);
+
+    renderProject(project);
+
+}
+export default firstRender
--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
 import Task from "./task.js"
 import Project from "./project"
 import renderProject from "./dom_functions.js"
+import firstRender from "./dom_functions.js"
 
-let task1 = new Task('A Do something','description1', new Date(2022,10,30), 2, false);
-let task2 = new Task('B Do something','description2', new Date(2022,10,28), 0, true);
-let task3 = new Task('D Do something','description3', new Date(2022,0,2), 1, false);
-let task4 = new Task('C Do something','description4', new Date(2022,11,2), 0, false);
-
-let project = new Project('projekty');
-
-project.addTask(task1);
-project.addTask(task2);
-project.addTask(task3);
-project.addTask(task4);
-
-renderProject(project);
-
+firstRender();
